f5b6eced35b046084f917da6350ca975580fbc41
I-make-gamez <ebelcher143@gmail.com>
2022-02-04 19:30:52 +0000

    add 02

--- core.js
+++ core.js
@@ -4,8 +4,15 @@ const c2 = document.querySelector('.two')
 const c3 = document.querySelector('.three')
 const cm = document.querySelector('.times')
 
-var input = " "
-var input2 = " "
+var input = [1, 2, 3]
+
+function solve(){
+    ot.innerHTML = eval(input[0], input[1], input[2]);
+}
+
+c1.addEventListener('click', function(){
+    input[0] = 1;
+})
 
 var o = {
     add: '+',
