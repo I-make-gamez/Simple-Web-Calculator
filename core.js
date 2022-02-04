e1be595a4a006f26cd915070b77297bc4ba777e3
I-make-gamez <ebelcher143@gmail.com>
2022-02-04 18:30:45 +0000

    add 01

--- core.js
+++ core.js
@@ -6,26 +6,10 @@ const cm = document.querySelector('.times')
 
 var input = " "
 var input2 = " "
-var opAdded = 0;
 
-c1.addEventListener('click', function(){
-    if(opAdded == 0){
-        input += 1;
-    }else {
-        input2 += 1
-    }
-    ot.value = Math.floor(input)
-})
-c2.addEventListener('click', function(){
-    input += 2;
-    ot.value = Math.floor(input)
-})
-
-c3.addEventListener('click', function(){
-    input += 3;
-    ot.value = Math.floor(input)
-})
-
-cm.addEventListener('click', function(){
-    opAdded = 3;
-})
+var o = {
+    add: '+',
+    sub: '-',
+    mul: '*',
+    div: '/'
+}
